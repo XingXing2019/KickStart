19f8227d9ff90718f0e65479fa1198031a306663
Xing <x.xing.unsw@gmail.com>
2021-04-09 16:35:25 +1000

    init

---
+++ 2018 Round E/Yogurt/Yogurt/Yogurt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Yogurt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cases = Convert.ToInt32(Console.ReadLine());
+			for (int i = 1; i <= cases; i++)
+			{
+				var line1 = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+				int N = line1[0], K = line1[1];
+				var yogurts = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+				int res = EatYogurt(K, yogurts);
+				Console.WriteLine($"Case #{i}: {res}");
+			}
+		}
+
+		private static int EatYogurt(int K, int[] yogurts)
+		{
+			int res = 0, index = 0, today = 0;
+			Array.Sort(yogurts);
+			while (index < yogurts.Length)
+			{
+				int count = 0;
+				while (index < yogurts.Length && count < K)
+				{
+					if (yogurts[index++] > today)
+					{
+						res++;
+						count++;
+					}
+				}
+				today++;
+			}
+			return res;
+		} 
+	}
+}
